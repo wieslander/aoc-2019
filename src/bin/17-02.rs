55bf982e4f26d6f6907f01ba1f087bff5383174d
Pär Wieslander <wieslander@gmail.com>
2019-12-17 13:23:08 +0100

    Day 17 part 2

--- src/bin/17-02.rs
+++ src/bin/17-02.rs
@@ -9,12 +9,6 @@ enum Direction {
     Down,
 }
 
-impl Direction {
-    pub fn all() -> Vec<Direction> {
-        vec![Direction::Up, Direction::Down, Direction::Left, Direction::Right]
-    }
-}
-
 enum Tile {
     Space,
     Scaffold,
@@ -28,10 +22,6 @@ struct Point {
 }
 
 impl Point {
-    pub fn manhattan_distance(&self, other: &Point) -> u32 {
-        ((self.x - other.x).abs() + (self.y - other.y).abs()) as u32
-    }
-
     pub fn step(&mut self, direction: &Direction) {
         match direction {
             Direction::Up => self.y -= 1,
@@ -40,52 +30,6 @@ impl Point {
             Direction::Right => self.x += 1,
         }
     }
-
-    pub fn point_in_direction(&self, direction: &Direction) -> Point {
-        let mut p = self.clone();
-        p.step(direction);
-        p
-    }
-
-    pub fn neighbors(&self) -> Vec<Point> {
-        Direction::all().iter().map(|dir| self.point_in_direction(dir)).collect()
-    }
-
-    pub fn direction(&self, other: &Point) -> Direction {
-        for dir in Direction::all() {
-            if self.point_in_direction(&dir) == *other {
-                return dir;
-            }
-        }
-
-        panic!("{:?} is not a neighbor of {:?}", other, self);
-    }
-}
-
-fn is_scaffold(point: &Point, grid: &HashMap<Point, Tile>) -> bool {
-    if let Some(tile) = grid.get(point) {
-        if let Tile::Scaffold = tile {
-            true
-        } else {
-            false
-        }
-    } else {
-        false
-    }
-}
-
-fn get_intersection_sum(grid: &HashMap<Point, Tile>) -> i32 {
-    let mut sum = 0;
-
-    for (pos, tile) in grid {
-        if let Tile::Scaffold = tile {
-            if pos.neighbors().iter().all(|&n| is_scaffold(&n, grid)) {
-                sum += pos.x * pos.y
-            }
-        }
-    }
-
-    sum
 }
 
 fn main() {
@@ -101,22 +45,25 @@ fn main() {
     let mut grid = HashMap::new();
     let mut pos = Point { x: 0, y: 0 };
 
-    let input_lines = vec![
-        "A,B\n",
-        "R,10,L,8,R,10\n",
-        "R,4,L,6,L,6,R,10\n",
-        "R\n",
+    let input = vec![
+        "A,B,A,C,B,C,A,B,A,C\n",
+        "R,10,L,8,R,10,R,4\n",
+        "L,6,L,6,R,10\n",
+        "L,6,R,12,R,12,R,10\n",
         "y\n",
-    ];
-    let input = input_lines.join("");
+    ].join("");
 
     for ch in input.chars() {
-        println!("{}", ch as i64);
         p.set_input(ch as i64);
     }
 
     while p.is_running() {
         if let Some(ch) = p.pause_on_output() {
+            if ch > 255 {
+                println!("Dust collected: {}", ch);
+                continue;
+            }
+
             let out = (ch as u8) as char;
             match out {
                 '\n' => {
@@ -147,6 +94,4 @@ fn main() {
             print!("{}", out);
         }
     }
-
-    println!("{}", get_intersection_sum(&grid));
 }
